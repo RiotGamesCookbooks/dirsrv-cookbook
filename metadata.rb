881b34e660712296d74554b67f1d6a5e74d5df96
Alan Willis <alwillis@riotgames.com>
2013-05-04 21:11:11 -0700

    Add maintainer info

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             '389ds'
+maintainer       'Alan Willis'
+maintainer_email 'alan@amekoshi.com'
+license          'All rights reserved'
+description      'Installs/Configures 389ds'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'yum', '~> 2.2'
+depends 'sysctl', '~> 0.3'
