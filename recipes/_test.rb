fc40a4fc0cd420838b1721d1fb93ae561e97a0b1
Alan Willis <alwillis@riotgames.com>
2013-09-04 02:53:07 +0100

    Update examples, drop credentials from dirsrv_entry and derivatives

--- recipes/_test.rb
+++ recipes/_test.rb
@@ -15,8 +15,6 @@ dirsrv_instance 'admin' do
   admin_user   "admin"
   admin_pass   "password"
   admin_port   9830
-  root_dn      'cn=Directory Manager'
-  root_pass    "password"
   port         388
   suffix       'o=testorg'
   action       [ :create, :start ]
@@ -27,51 +25,32 @@ dirsrv_instance 'test' do
   admin_pass   "password"
   admin_port   9830
   admin_host   node[:ipaddress]
-  root_dn      'cn=Directory Manager'
-  root_pass    'password'
   port         389
   suffix       'o=testorg'
   action       [ :create, :start ]
 end
 
 dirsrv_entry 'ou=test,o=testorg' do
-  host        node[:ipaddress]
   port        389
-  userdn     'cn=Directory Manager'
-  pass       'password'
   attributes  ({ objectClass: [ 'top', 'organizationalUnit' ], l: [ 'PA', 'CA' ], telephoneNumber: '215-310-5555' })
   prune      ([ :postalCode, :description ])
 end
 
 dirsrv_config "nsslapd-auditlog-logging-enabled" do
-  userdn 'cn=Directory Manager'
-  pass   'password'
   value  'on'
 end
 
 dirsrv_config "nsslapd-auditlog" do
-  userdn 'cn=Directory Manager'
-  pass   'password'
   value  '/var/log/dirsrv/slapd-test/audit'
 end
 
-dirsrv_plugin "MemberOf Plugin" do
-  userdn 'cn=Directory Manager'
-  pass   'password'
-end
+dirsrv_plugin "MemberOf Plugin"
 
-dirsrv_plugin "Posix Winsync API" do
-  userdn 'cn=Directory Manager'
-  pass   'password'
+dirsrv_plugin "Posix Winsync API"
   attributes ({ posixwinsynccreatememberoftask: 'true' })
 end
 
 dirsrv_plugin "referential integrity postoperation" do
-  userdn     'cn=Directory Manager'
-  pass       'password'
   attributes ({ :'nsslapd-pluginEnabled' => 'on' })
   action     :modify
 end
-
-cert = Chef::DirsrvCertificate.new
-p cert.load
