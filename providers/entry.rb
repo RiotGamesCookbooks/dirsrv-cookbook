b6b3407ff6e45dba150b79503334bc7b37dfed6f
Alan Willis <alwillis@riotgames.com>
2014-07-07 12:49:16 +0100

    remove confusing 'modify_entry' method, same name as one of the Chef::Dirsrv methods

--- providers/entry.rb
+++ providers/entry.rb
@@ -21,7 +21,98 @@ action :create do
   end
 
   converge_by("Entry #{@new_resource.dn}") do
-    modify_entry
+
+    dirsrv = Chef::Dirsrv.new
+
+    if @current_resource.nil?
+      Chef::Log.info("Adding #{@new_resource.dn}")
+      dirsrv.add_entry(@new_resource)
+      new_resource.updated_by_last_action(true)
+    else
+
+      current_attribute_names = @current_resource.attribute_names.map{ |k| k.downcase.to_s }
+
+      # Include seed attributes in with the normal attributes as long as they don't already exist
+      @new_resource.seed_attributes.keys.each{ |k| @new_resource.seed_attributes[k.downcase.to_s] = @new_resource.seed_attributes.delete(k) }
+      ( @new_resource.seed_attributes.keys - current_attribute_names ).map{ |attr|
+        @new_resource.attributes.merge!({ attr => @new_resource.seed_attributes[attr].is_a?(String) ? [ @new_resource.seed_attributes[attr] ] : @new_resource.seed_attributes[attr] })
+      }
+
+      all_attributes = @new_resource.attributes.merge(@new_resource.append_attributes)
+
+      # Add keys that are missing
+      add_keys = ( all_attributes.keys - current_attribute_names ).map{ |attr|
+        [ :add, attr, all_attributes[attr].is_a?(String) ? [ all_attributes[attr] ] : all_attributes[attr] ]
+      }
+
+      # Update existing keys, append values if necessary
+      update_keys = Array.new
+
+      ( all_attributes.keys & current_attribute_names ).each do |attr|
+
+        # Ignore Distinguished Name (DN) and the Relative DN. 
+        # These should only be modified upon entry creation to avoid schema violations
+        rdn = @new_resource.dn.split('=').first
+        next if attr =~ /DN/i || attr <=> rdn 
+
+        if @new_resource.append_attributes[attr]
+
+          append_values = @new_resource.append_attributes[attr].is_a?(String) ? [ @new_resource.append_attributes[attr] ] : @new_resource.append_attributes[attr]
+          append_values -= @current_resource.send(attr)
+
+          if append_values.size > 0 
+            update_keys.push([ :add, attr, append_values ])
+          end
+        end
+
+        if @new_resource.attributes[attr]
+
+          replace_values = @new_resource.attributes[attr].is_a?(String) ? [ @new_resource.attributes[attr] ] : @new_resource.attributes[attr]
+
+          if ( replace_values.size > 0 ) and ( replace_values != @current_resource.send(attr) )
+            update_keys.push([ :replace, attr, replace_values ])
+          end
+        end
+      end
+
+      # Prune unwanted attributes and/or values
+      prune_keys = Array.new
+      if @new_resource.prune.is_a?(Array)
+        @new_resource.prune.each do |attr|
+          next unless @current_resource.respond_to?(attr)
+          prune_keys.push([ :delete, attr, nil ])
+        end
+      elsif @new_resource.prune.is_a?(Hash)
+        @new_resource.prune.each do |attr, values|
+          next unless @current_resource.respond_to?(attr)
+          values = values.is_a?(String) ? [ values ] : values
+          values = ( values & @current_resource.send(attr) )
+          prune_keys.push([ :delete, attr, values ]) if values.size > 0
+        end
+      end
+
+      # Modify entry if there are any changes to be made
+      if ( add_keys | update_keys | prune_keys ).size > 0
+        # Submit one set of operations at a time, easier to debug
+
+        if add_keys.size > 0
+          Chef::Log.info("Add #{@new_resource.dn} #{ add_keys }")
+          dirsrv.modify_entry(@new_resource, add_keys)
+        end
+
+        if update_keys.size > 0
+          Chef::Log.info("Update #{@new_resource.dn} #{update_keys}")
+          dirsrv.modify_entry(@new_resource, update_keys)
+        end
+
+        if prune_keys.size > 0
+          Chef::Log.info("Delete #{@new_resource.dn} #{prune_keys}")
+          dirsrv.modify_entry(@new_resource, prune_keys)
+        end
+
+        new_resource.updated_by_last_action(true)
+      end
+    end
   end
 end
 
@@ -42,99 +133,4 @@ def load_current_resource
   dirsrv = Chef::Dirsrv.new
   @current_resource = dirsrv.get_entry(@new_resource)
   @current_resource
-end
-
-def modify_entry
-
-  dirsrv = Chef::Dirsrv.new
-
-  if @current_resource.nil?
-    Chef::Log.info("Adding #{@new_resource.dn}")
-    dirsrv.add_entry(@new_resource)
-    new_resource.updated_by_last_action(true)
-  else
-
-    current_attribute_names = @current_resource.attribute_names.map{ |k| k.downcase.to_s }
-
-    # Include seed attributes in with the normal attributes as long as they don't already exist
-    @new_resource.seed_attributes.keys.each{ |k| @new_resource.seed_attributes[k.downcase.to_s] = @new_resource.seed_attributes.delete(k) }
-    ( @new_resource.seed_attributes.keys - current_attribute_names ).map{ |attr|
-      @new_resource.attributes.merge!({ attr => @new_resource.seed_attributes[attr].is_a?(String) ? [ @new_resource.seed_attributes[attr] ] : @new_resource.seed_attributes[attr] })
-    }
-
-    all_attributes = @new_resource.attributes.merge(@new_resource.append_attributes)
-
-    # Add keys that are missing
-    add_keys = ( all_attributes.keys - current_attribute_names ).map{ |attr|
-      [ :add, attr, all_attributes[attr].is_a?(String) ? [ all_attributes[attr] ] : all_attributes[attr] ]
-    }
-
-    # Update existing keys, append values if necessary
-    update_keys = Array.new
-
-    ( all_attributes.keys & current_attribute_names ).each do |attr|
-
-      # Ignore Distinguished Name (DN) and the Relative DN. 
-      # These should only be modified upon entry creation to avoid schema violations
-      rdn = @new_resource.dn.split('=').first
-      next if attr =~ /DN/i || attr <=> rdn 
-
-      if @new_resource.append_attributes[attr]
-
-        append_values = @new_resource.append_attributes[attr].is_a?(String) ? [ @new_resource.append_attributes[attr] ] : @new_resource.append_attributes[attr]
-        append_values -= @current_resource.send(attr)
-
-        if append_values.size > 0 
-          update_keys.push([ :add, attr, append_values ])
-        end
-      end
-
-      if @new_resource.attributes[attr]
-
-        replace_values = @new_resource.attributes[attr].is_a?(String) ? [ @new_resource.attributes[attr] ] : @new_resource.attributes[attr]
-
-        if ( replace_values.size > 0 ) and ( replace_values != @current_resource.send(attr) )
-          update_keys.push([ :replace, attr, replace_values ])
-        end
-      end
-    end
-
-    # Prune unwanted attributes and/or values
-    prune_keys = Array.new
-    if @new_resource.prune.is_a?(Array)
-      @new_resource.prune.each do |attr|
-        next unless @current_resource.respond_to?(attr)
-        prune_keys.push([ :delete, attr, nil ])
-      end
-    elsif @new_resource.prune.is_a?(Hash)
-      @new_resource.prune.each do |attr, values|
-        next unless @current_resource.respond_to?(attr)
-        values = values.is_a?(String) ? [ values ] : values
-        values = ( values & @current_resource.send(attr) )
-        prune_keys.push([ :delete, attr, values ]) if values.size > 0
-      end
-    end
-
-    # Modify entry if there are any changes to be made
-    if ( add_keys | update_keys | prune_keys ).size > 0
-      # Submit one set of operations at a time, easier to debug
-
-      if add_keys.size > 0
-        Chef::Log.info("Add #{@new_resource.dn} #{ add_keys }")
-        dirsrv.modify_entry(@new_resource, add_keys)
-      end
-
-      if update_keys.size > 0
-        Chef::Log.info("Update #{@new_resource.dn} #{update_keys}")
-        dirsrv.modify_entry(@new_resource, update_keys)
-      end
-
-      if prune_keys.size > 0
-        Chef::Log.info("Delete #{@new_resource.dn} #{prune_keys}")
-        dirsrv.modify_entry(@new_resource, prune_keys)
-      end
-
-      new_resource.updated_by_last_action(true)
-    end
-  end
 end
