223d5740d146232b51717d34d7ba10a18c0507d6
Alan Willis <alan@amekoshi.com>
2014-08-19 23:17:37 +0100

    first cut at admin lwrp

---
+++ providers/admin.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: dirsrv
+# Provider:: admin
+#
+# Copyright 2014, Alan Willis <alwillis@riotgames.com>
+#
+# All rights reserved
+#
+
+def whyrun_supported?
+  true
+end
+
+#%admpw%
+#%as_access%
+#%as_addr%
+#%as_buildnum%
+#%as_console_jar%
+#%as_error%
+#%as_help_path%
+#%asid%
+#%as_pid%
+#%as_port%
+#%as_sie%
+#%as_user%
+#%as_version%
+#%brand%
+#%domain%
+#%fqdn%
+#%timestamp%
+#%uname_a%
+#%uname_m%
+#%vendor%
+
