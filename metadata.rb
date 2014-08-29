87819e0e1bac9db68d9d2cf89f54465607e58781
Alan Willis <alwillis@riotgames.com>
2014-08-29 12:47:43 +0100

    fix broken ldap cookbook dependency

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,9 @@ description      'Installs and configures 389 Directory Server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'ldap', '~> 1.0.0'
+depends 'ldap', '~> 1.0'
 depends 'yum', '~> 3.0'
 depends 'hostsfile'
-depends 'ldap'
 
 supports 'centos'
 supports 'fedora'
