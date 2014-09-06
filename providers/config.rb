25df73864bfc3dd551e4238be917c2106ae9174a
Alan Willis <alan@amekoshi.com>
2014-09-06 13:27:57 +0100

    no real way to disable cn=config options, change default action name to :set

--- providers/config.rb
+++ providers/config.rb
@@ -24,7 +24,7 @@ def whyrun_supported?
   true
 end
 
-action :enable do
+action :set do
 
   converge_by("Setting #{new_resource.attr}: #{new_resource.value}") do
     ldap_entry new_resource.attr do
@@ -34,20 +34,6 @@ action :enable do
       credentials new_resource.credentials
       databag_name new_resource.databag_name
       attributes ({ new_resource.attr.to_sym => new_resource.value.to_s })
-    end
-  end
-end
-
-action :disable do
-
-  converge_by("Unsetting #{new_resource.attr}") do
-    ldap_entry new_resource.attr do
-      distinguished_name 'cn=config'
-      host   new_resource.host
-      port   new_resource.port
-      credentials new_resource.credentials
-      databag_name new_resource.databag_name
-      prune  new_resource.value ? { new_resource.attr.to_sym => new_resource.value.to_s } : [ new_resource.attr.to_sym ]
     end
   end
 end
