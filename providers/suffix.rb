c26512156c81179fdbc154e3ad4814983e08d5e5
Alan Willis <alwillis@riotgames.com>
2015-07-17 10:41:04 -0700

    Add new suffix provider

---
+++ providers/suffix.rb
@@ -0,0 +1,72 @@
+#
+# Cookbook Name:: dirsrv
+# Provider:: suffix
+#
+# Copyright 2015 Riot Games, Inc.
+# Author:: Alan Willis <alwillis@riotgames.com>
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+use_inline_resources
+
+def whyrun_supported?
+  true
+end
+
+action :create do
+
+  converge_by("Creating new suffix #{new_resource.suffix}") do
+
+    # ldbm database
+
+    backend_name = new_resource.nsslapd_backend.nil? ? new_resource.suffix.gsub(/(,|=)/, '_') : new_resource.nsslapd_backend
+
+    ldap_entry "cn=#{backend_name},cn=ldbm database,cn=plugins,cn=config" do
+      host   new_resource.host
+      port   new_resource.port
+      credentials new_resource.credentials
+      databag_name new_resource.databag_name
+      attributes({ 'objectClass' => [ 'top', 'extensibleObject', 'nsBackendInstance' ] })
+      seed_attributes({ 'nsslapd-suffix' => new_resource.suffix.to_s })
+    end
+
+    # mapping tree
+
+    maptree_attrs = { 'nsslapd-state' => 'backend', 'nsslapd-backend' => backend_name }
+
+    if new_resource.parent
+      maptree_attrs.merge!({ "nsslapd-parent-suffix" => new_resource.parent.to_s })
+    end
+
+    ldap_entry "cn=\"#{new_resource.suffix}\",cn=mapping tree,cn=config" do
+      host   new_resource.host
+      port   new_resource.port
+      credentials new_resource.credentials
+      databag_name new_resource.databag_name
+      attributes({ 'objectClass' => [ 'top', 'extensibleObject', 'nsMappingTree' ], 
+                   'cn' => new_resource.suffix })
+      seed_attributes maptree_attrs
+    end
+
+    # suffix entry
+
+    ldap_entry new_resource.suffix do
+      host   new_resource.host
+      port   new_resource.port
+      credentials new_resource.credentials
+      databag_name new_resource.databag_name
+      attributes({ 'objectClass' => [ 'top', 'extensibleObject' ] })
+    end
+  end
+end
---
+++ resources/suffix.rb
@@ -0,0 +1,29 @@
+# Cookbook Name:: dirsrv
+# Resource:: suffix
+#
+# Copyright 2015 Riot Games, Inc.
+# Author:: Alan Willis <alwillis@riotgames.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+actions :create
+default_action :create
+
+attribute :suffix, :kind_of => String, :name_attribute => true
+attribute :parent, :kind_of => String
+attribute :nsslapd_backend, :kind_of => String
+attribute :host, :kind_of => String, :default => 'localhost'
+attribute :port, :kind_of => Integer, :default => 389
+attribute :credentials, :kind_of => [ String, Hash ], :default => 'default_credentials'
+attribute :databag_name, :kind_of => String
