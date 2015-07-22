0f0fa407c1a895e0b7213190e2e309972df1ffb4
Alan Willis <alwillis@riotgames.com>
2015-07-22 13:16:47 -0700

    dirsrv_config should not accept arrays

--- resources/config.rb
+++ resources/config.rb
@@ -21,7 +21,7 @@ actions :set
 default_action :set
 
 attribute :attr, :kind_of => String, :name_attribute => true
-attribute :value, :kind_of => [ String, Array ], :default => []
+attribute :value, :kind_of => [ String, Integer ], :required => true
 attribute :host, :kind_of => String, :default => 'localhost'
 attribute :port, :kind_of => Integer, :default => 389
 attribute :credentials, :kind_of => [ String, Hash ], :default => 'default_credentials'
